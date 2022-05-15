59535d1283b77c82dcac11aa5be4d3c193060aed
doraneko <morita4492.0305@gmail.com>
2022-05-16 08:36:15 +0900

    add

--- lib/components/user/user.dart
+++ lib/components/user/user.dart
@@ -3,13 +3,10 @@ import '../importer.dart';
 class UserPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CupertinoSearchTextField(
-      onChanged: (String value) {
-        print('The text has changed to: $value');
-      },
-      onSubmitted: (String value) {
-        print('Submitted text: $value');
-      },
-    );
+    CupertinoNavigationBar(
+          trailing: Icon(CupertinoIcons.settings),
+          middle: Text("Title"),
+          leading: Icon(CupertinoIcons.back),
+        ),
   }
 }
