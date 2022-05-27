953d88bf7615b39d34d663255ad01237ca3bb8e3
doraneko <doraneko1600@gmail.com>
2022-05-27 11:08:10 +0900

    debug macos build

--- lib/models/timecard.dart
+++ lib/models/timecard.dart
@@ -1,10 +1,14 @@
 import 'package:flutter_application_1/components/importer.dart';
 
 void timeCardSetDB(String title, String id) {
-  String year = "1111";
+  // 現在の日時取得
+  DateTime now = DateTime.now();
+
+  // 現在の日時から年、月日を抽出。文字列にキャスト
+  String year = now.year.toString();
   String days = "0111";
   int time = 1220;
-  
+
   FirebaseFirestore.instance
       .collection(SettingsFirebase.collection)
       .doc(id)
