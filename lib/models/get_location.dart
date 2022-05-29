706aa32d1bf6689710969bb51e4cb0c06dd4a498
doraneko <morita4492.0305@gmail.com>
2022-05-30 08:32:39 +0900

    debug

--- lib/models/get_location.dart
+++ lib/models/get_location.dart
@@ -2,7 +2,8 @@ import 'package:flutter_application_1/components/importer.dart';
 
 String getLocation() {
   
-  String location = determinePosition() as String;
+  // String location = determinePosition() as String;
+  String location = Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high) as String ;
 
   return location;
 }
