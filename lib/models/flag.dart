7e3888f9a01b3c35d8c4460bb50d9de897b7a67d
doraneko <morita4492.0305@gmail.com>
2022-06-08 00:22:36 +0900

    add

--- lib/models/flag.dart
+++ lib/models/flag.dart
@@ -2,7 +2,7 @@ import 'package:flutter_application_1/components/importer.dart';
 
 int flagManagement() {
   FirebaseFirestore.instance.collection(SettingsFirebase.collection)
-      .doc(id).snapshots().listen((DocumentSnapshot snapshot) {
+      .doc('1').snapshots().listen((DocumentSnapshot snapshot) {
       print(snapshot.get('name'));
 });
   return 0;
